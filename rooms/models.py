6a69bc85f632d8405dc29e6763030d0aeb3c9a65
Seungkee Chae <seungkeechae@Seungkeeui-MacBookPro.local>
2022-05-25 10:13:35 +0900

    4.4 on_delete,Amenity,Facility,HouseRule add many for rooms@@

--- rooms/models.py
+++ rooms/models.py
@@ -17,7 +17,27 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
+
+    """RoomType Model Definition"""
+
     pass
+
+
+class Amenity(AbstractItem):
+    """Amenity Model Definition"""
+
+    class Meta:
+        pass
+
+
+class HouseRule(AbstractItem):
+    """HouseRule Model Definition"""
+
+    pass
+
+
+class Facility(AbstractItem):
+    """Facility Model Definition"""
 
 
 class Room(core_models.TimeStampedModel):
@@ -38,7 +58,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenitys = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
