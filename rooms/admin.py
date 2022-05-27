4636aa3937b9b5e43012329d989b6adaa3e178f6
Seungkee Chae <seungkeechae@Seungkeeui-MacBookPro.local>
2022-05-27 21:30:43 +0900

    7.0 ~ 7.3 related_name, python manage.py shell, so fantastic lecture

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,6 +6,11 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """Item Admin Definition"""
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -53,6 +58,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -66,14 +72,15 @@ class RoomAdmin(admin.ModelAdmin):
         "country",
     )
 
-    def count_amenities(self, obj):
-        return obj.amenities.count()
-
-    count_amenities.short_description = "gogogo"
-
     search_fields = ("city", "^host__username")
 
     filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
