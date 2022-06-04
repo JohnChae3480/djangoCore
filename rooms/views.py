f838f877607e4034b0d383c9dc3003da49d6dfa1
Seungkee Chae <seungkeechae@Seungkeeui-MacBookPro.local>
2022-06-05 00:38:55 +0900

    12.0 ~ 12.4 DetailView wow..

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -17,6 +17,20 @@ class HomeView(ListView):
         now = timezone.now()
         context["now"] = now
         return context
+
+
+class RoomDetail(DetailView):
+    """RoomDetail Definition"""
+
+    model = models.Room
+
+
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
 
 
 # def all_rooms(request):
